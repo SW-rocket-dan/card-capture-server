b5faaee617eb1e38c28564e03c4a873b2e57a883
inhyeok <ss112d86@gmail.com>
2025-11-08 21:09:49 +0900

    Refactor: S3Config nullable 제거

--- platform/ai/adapter/src/main/kotlin/app/cardcapture/ai/outbound/s3/S3Config.kt
+++ platform/ai/adapter/src/main/kotlin/app/cardcapture/ai/outbound/s3/S3Config.kt
@@ -10,20 +10,20 @@ import software.amazon.awssdk.services.s3.S3Client
 
 
 @Configuration
-class S3Config {
-
-
+class S3Config(
     @Value("\${cloud.aws.credentials.access-key}")
-    private val accessKey: String? = null
+    private val accessKey: String,
 
     @Value("\${cloud.aws.credentials.secret-key}")
-    private val secretKey: String? = null
+    private val secretKey: String,
 
     @Value("\${cloud.aws.region.static}")
-    private val region: String? = null
+    private val region: String,
+) {
+
 
     @Bean
-    fun amazonS3(): S3Client? {
+    fun amazonS3(): S3Client {
         val credentials = AwsBasicCredentials.create(accessKey, secretKey)
         return S3Client.builder()
             .region(Region.of(region))
